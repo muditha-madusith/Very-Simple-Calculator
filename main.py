c645ccb969e021e1be70bfcb4f087718bbc2da07
Muditha Madusith <bmmadusith2003@gmail.com>
2022-07-28 12:59:17 +0530

    Add the files

---
+++ main.py
@@ -0,0 +1,14 @@
+fN = int(input("Enter Your first number:- "))
+Function = str(input("Enter the sum (+,-,/,*):- "))
+sN = int(input("Enter the second number:- "))
+
+if Function == "+":
+    output = fN + sN
+if Function == "-":
+    output = fN - sN
+if Function == "*":
+    output = fN * sN
+if Function == "/":
+    output = fN / sN
+
+print("Answer is :",output)
